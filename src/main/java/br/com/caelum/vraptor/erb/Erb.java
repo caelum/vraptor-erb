eaf701edc321c74b2aecef7e2730b2689666bd33
guilherme e joviane <guilherme.silveira@caelum.com.br>
2013-09-12 17:59:26 -0300

    info

--- src/main/java/br/com/caelum/vraptor/erb/Erb.java
+++ src/main/java/br/com/caelum/vraptor/erb/Erb.java
@@ -1,46 +1,57 @@
 package br.com.caelum.vraptor.erb;
 
+import java.io.File;
 import java.net.URL;
 import java.util.Map;
 
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+
+import org.jruby.embed.ScriptingContainer;
 
 public class Erb {
 	
+	static ScriptingContainer container = new ScriptingContainer();
+	static {
+		
+		container.runScriptlet("class Object\n" +
+				"	alias_method :require_original, :require\n" +
+				"end\n");
+		container.runScriptlet("require 'erb'");
+		String gemPath = new File("gems/").getAbsolutePath();
+		container.runScriptlet(
+				"def require(name)\n" +
+				"	begin\n"+
+				"		x = require_original name\n" +
+				"		return x\n" + 
+				"	rescue LoadError\n" +
+				"		$LOAD_PATH << \"" + gemPath + "/#{name}/lib\"\n" +
+				"		require_original name\n" +
+				"	end\n" +
+				"end\n");
+		container.runScriptlet(
+				"class MyClass\n" +
+				"end\n");
+
+	}
+	
 	public String render(String name, Map<String, Object> map) throws ScriptException {
-		ScriptEngineManager m = new ScriptEngineManager();
-		ScriptEngine rubyEngine = m.getEngineByName("jruby");
-		if(rubyEngine==null) {
-			throw new RuntimeException("Unable to load jruby");
-		}
-
-		ScriptContext context = rubyEngine.getContext();
-
 		StringBuilder argsNames = new StringBuilder();
-		StringBuilder argsVariables = new StringBuilder();
 		for(String key : map.keySet()) {
-			context.setAttribute(key, map.get(key), ScriptContext.ENGINE_SCOPE);
+			container.put(key, map.get(key));
 			argsNames.append(key + ",");
-			argsVariables.append("$"+key + ",");
 		}
 		String args = argsNames.length()==0? "" : argsNames.substring(0, argsNames.length()-1);
-		String args2 = argsVariables.length()==0? "" : argsVariables.substring(0, argsVariables.length()-1);
 		
 		URL resource = Erb.class.getResource("/" +name);
 		String filename =  resource.toExternalForm();
-		context.setAttribute("filename", filename, ScriptContext.ENGINE_SCOPE);
-
-		return rubyEngine.eval(
-				"require 'erb'\n" + 
-				"class MyClass;end\n" +
-				"template = File.read($filename)\n" +
+		container.put("filename", filename);
+		Object result = container.runScriptlet(
+				"template = File.read(filename)\n" +
 				"erb = ERB.new(template)\n" +
-				"erb.filename = $filename\n"+
-				"erb.def_method(MyClass, 'render(" + args + ")', $filename)\n" + 
-				"result = MyClass.new.render(" + args2 + ")", context).toString();
+				"erb.filename = filename\n"+
+				"erb.def_method(MyClass, 'render(" + args + ")', filename)\n" + 
+				"result = MyClass.new.render(" + args + ")");
+		return result.toString();
 	}
 
 }
